4299bc4ed40c0707b4b7bfc25749585d98ab5567
Paweł Wójcik <avedonmedia@gmail.com>
2024-12-21 16:40:12 +0100

    add base project structure

--- main.go
+++ main.go
@@ -1,5 +1,31 @@
 package main
 
+type ScoreBoard struct{}
+
+type Team struct {
+	name   string
+	scores int
+}
+
+type Game struct {
+	homeTeam Team
+	awayTeam Team
+}
+
+var games []Game
+
 func main() {
 
+}
+
+func NewScoreBoard() ScoreBoard {
+	return ScoreBoard{}
+}
+
+func NewTeam(name string) *Team {
+	return &Team{name, 0}
+}
+
+func NewGame(homeTeam, awayTeam Team) *Game {
+	return &Game{homeTeam, awayTeam}
 }
