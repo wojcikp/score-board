c7083e2c8d5371806febb31b7e97e2608db34bde
Paweł Wójcik <avedonmedia@gmail.com>
2024-12-22 19:38:27 +0100

    add start new game functionality

--- main.go
+++ main.go
@@ -86,12 +86,106 @@ func initBoard() {
 	}
 }
 
-
 func (b ScoreBoard) startNewGame() {
-	clearConsole()
-	fmt.Println("Start new game")
-	time.Sleep(2 * time.Second)
-	return
+	homeTeamName, awayTeamName := getTeamNames()
+	homeTeam := NewTeam(homeTeamName)
+	awayTeam := NewTeam(awayTeamName)
+	game := NewGame(*homeTeam, *awayTeam)
+	for {
+		clearConsole()
+		scanner := bufio.NewScanner(os.Stdin)
+		fmt.Printf("%s: %d vs. %s: %d\n", homeTeam.name, homeTeam.scores, awayTeam.name, awayTeam.scores)
+		fmt.Println("Options: ")
+		fmt.Printf("1. %s scored!\n", homeTeam.name)
+		fmt.Printf("2. %s scored!\n", awayTeam.name)
+		fmt.Printf("3. Remove one point from the %s team\n", homeTeam.name)
+		fmt.Printf("4. Remove one point from the %s team\n", awayTeam.name)
+		fmt.Println("5. Finish game")
+
+		scanner.Scan()
+		input := scanner.Text()
+		input = strings.TrimSpace(input)
+		choice, err := strconv.Atoi(input)
+		if err != nil || choice < 1 || choice > 5 {
+			fmt.Println("Invalid input. Please enter a number between 1 and 5.")
+			fmt.Println("Press Enter to continue...")
+			scanner.Scan()
+			continue
+		}
+
+		switch choice {
+		case 1:
+			homeTeam.addOnePoint()
+			continue
+		case 2:
+			awayTeam.addOnePoint()
+			continue
+		case 3:
+			homeTeam.removeOnePoint()
+			continue
+		case 4:
+			awayTeam.removeOnePoint()
+			continue
+		case 5:
+			fmt.Println("Finishing game...")
+			games = append(games, *game)
+			time.Sleep(3 * time.Second)
+			return
+		default:
+			fmt.Println("Invalid input. Please enter a number between 1 and 5.")
+			fmt.Println("Press Enter to continue...")
+			scanner.Scan()
+			continue
+		}
+	}
+}
+
+func getTeamNames() (string, string) {
+	for {
+		clearConsole()
+		scanner := bufio.NewScanner(os.Stdin)
+		fmt.Println("Start new game")
+		fmt.Println("-------------------------")
+
+		fmt.Println("Enter a name of the Home team and press Enter:")
+		scanner.Scan()
+		homeTeamName := scanner.Text()
+
+		fmt.Println("Enter a name of the Away team and press Enter:")
+		scanner.Scan()
+		awayTeamName := scanner.Text()
+
+		fmt.Println("The game is about to start")
+		fmt.Println("Home team: ", homeTeamName)
+		fmt.Println("Away team: ", awayTeamName)
+		fmt.Println("Choose an option:")
+		fmt.Println("1. Team names are correct, let's start the game!")
+		fmt.Println("2. Team names are incorrect, let's correct them.")
+
+		scanner.Scan()
+		input := scanner.Text()
+		input = strings.TrimSpace(input)
+		choice, err := strconv.Atoi(input)
+		if err != nil || choice < 1 || choice > 2 {
+			fmt.Println("Invalid input. Please enter a number 1 or 2.")
+			fmt.Println("Press Enter to continue...")
+			scanner.Scan()
+			continue
+		}
+
+		switch choice {
+		case 1:
+			return homeTeamName, awayTeamName
+		case 2:
+			fmt.Println("Correcting team names...")
+			time.Sleep(3 * time.Second)
+			continue
+		default:
+			fmt.Println("Invalid input. Please enter a number 1 or 2.")
+			time.Sleep(3 * time.Second)
+			continue
+		}
+	}
 }
 
 func (b ScoreBoard) getSummaryOfGames() {
@@ -106,6 +200,16 @@ func (b ScoreBoard) updateGameScore() {
 	fmt.Println("Update game score")
 	time.Sleep(2 * time.Second)
 	return
+}
+
+func (t *Team) addOnePoint() {
+	t.scores++
+}
+
+func (t *Team) removeOnePoint() {
+	if t.scores > 0 {
+		t.scores--
+	}
 }
 
 func clearConsole() {
